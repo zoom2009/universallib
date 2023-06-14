c2928748e0034930be11d4bdfc3cebacc1c46b30
afu-hive <afu.sikarinp@hivecorps.com>
2023-06-14 20:54:08 +0700

    f

--- packages/app/components/Toast.tsx
+++ packages/app/components/Toast.tsx
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native'
 import { ToastContainer } from 'react-toast'
 import React from 'react'
-import { TOAST_DURATION } from 'app/functions/displayToast/index.native'
 
 interface IToastRootProviderProps {
   children: React.ReactNode
@@ -11,7 +10,7 @@ export const ToastRootProvider = (props: IToastRootProviderProps) => {
   if (Platform.OS === 'web') return (
     <>
       {props.children}
-      <ToastContainer delay={TOAST_DURATION} />
+      <ToastContainer delay={4500} />
     </>
   )
 
--- packages/app/functions/displayToast/index.native.ts
+++ packages/app/functions/displayToast/index.native.ts
@@ -1,12 +1,10 @@
 import Toast from 'react-native-root-toast'
 import { IDisplayToastProps } from './interface'
 
-export const TOAST_DURATION = 4500
-
 export const displayToast = (props: IDisplayToastProps) => {
   const { message } = props
   Toast.show(message, {
-    duration: TOAST_DURATION,
+    duration: 4500,
     delay: 0,
     position: Toast.positions.BOTTOM,
     shadow: true,
