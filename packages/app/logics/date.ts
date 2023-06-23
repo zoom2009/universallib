930d533d26994ed29d29ae8eed297f7025a5659f
afu-hive <afu.sikarinp@hivecorps.com>
2023-06-23 12:54:36 +0700

    ด

--- packages/app/logics/date.ts
+++ packages/app/logics/date.ts
@@ -15,7 +15,7 @@ export const getThaiYear = (dateTimestamp?: number) => {
 }
 
 export const getThaiMonth = (dateTimestamp?: number) => {
-  return d(dateTimestamp).format('MMMM')
+  return d(dateTimestamp).format('MMM')
 }
 
 export const getThaiDay = (dateTimestamp?: number) => {
@@ -57,4 +57,4 @@ export const getYearList = () => {
 
 export const getDayList = () => ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
-export const getMonthList = () => ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม']
+export const getMonthList = () => ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.']
